035874e4856d6d33946657d68ab27592274559d4
HenryBridges <henrybridges3@gmail.com>
2022-02-07 16:16:26 +0000

    first commmit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";  
+import { View, Text } from "react-native"; 
+
+const App = () => {
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text>
+        Hello World!
+      </Text>
+    </View>
+  )
+}
+
+export default App
