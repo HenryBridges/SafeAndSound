acd789758591ec3303a7f08db47c2202539c6604
HenryBridges <henrybridges3@gmail.com>
2022-03-04 15:38:12 +0000

    Changed Proj Name

--- ios/SafeSoundTests/SafeTests.m
+++ ios/SafeSoundTests/SafeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface safesoundTests : XCTestCase
+@interface SafeTests : XCTestCase
 
 @end
 
-@implementation safesoundTests
+@implementation SafeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
