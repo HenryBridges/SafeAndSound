3ca7caaa5a7858fca7b18bb1d2c9b72e150bcef0
HenryBridges <henrybridges3@gmail.com>
2022-03-17 18:29:51 +0000

    Input added

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Text, View, Dimensions, StyleSheet } from 'react-native';
+import { Text, View, Dimensions, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
 import Button from '../components/Buttons/Button';
+import UserLoginReq from '../models/User/UserLoginReq';
 
 const Login = ({ navigation }) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
   return (
     <>
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Email:'
+          placeholderTextColor='black'
+          onChangeText={(email) => setUserEmail(email)}
+        />
+      </View>
     </>
   );
 };
@@ -16,6 +27,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fef2f2'
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 2,
+    paddingVertical: 2,
+    paddingHorizontal: 10
   }
 });
 
