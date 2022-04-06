d40c936ff8639a35143bb7955f264222061a80f3
HenryBridges <henrybridges3@gmail.com>
2022-04-06 18:03:04 +0100

    added some design tweaks to appDrawer file

--- navigation/AppDrawer.js
+++ navigation/AppDrawer.js
@@ -4,13 +4,34 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home';
 import Stats from '../screens/Stats';
 import Report from '../screens/ReportModal';
-
+import gc from '../general/globalColors';
+import { Dimensions } from 'react-native';
+import { openEye } from '../assets/images/images';
+import { black } from 'react-native-paper/lib/typescript/styles/colors';
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 const Drawer = createDrawerNavigator();
 
 const AppDrawer = () => (
-  <Drawer.Navigator screenOptions={{ headerShown: false }}>
+  <Drawer.Navigator
+    screenOptions={{
+      headerShown: false,
+      drawerActiveTintColor: gc.colors.white,
+      drawerActiveBackgroundColor: gc.colors.periwinkle,
+      drawerInactiveTintColor: gc.colors.lightPeriwinkle,
+      drawerInactiveBackgroundColor: gc.colors.white,
+      drawerStyle: {
+        backgroundColor: gc.colors.white,
+        width: 0.6 * width
+      }
+    }}
+  >
     <Drawer.Screen name='Home' component={Home} />
-    <Drawer.Screen name='Stats' component={Stats} />
+    <Drawer.Screen
+      name='Stats'
+      component={Stats}
+      options={{ title: 'Statistics' }}
+    />
   </Drawer.Navigator>
 );
 
